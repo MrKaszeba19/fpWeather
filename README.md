6914aaa47f8e331a0d42bbef331f5f8d5c4d9d3d
RooiGevaar19 <redthreat19@gmail.com>
2021-11-01 10:17:36 +0100

    Add wind directions

--- README.md
+++ README.md
@@ -21,16 +21,27 @@ Author: Paul Lipkowski
 - `fpweather [flags]`
     * `-c` or `--config` – Launch config
     * `-h` or `--help` – Display help
-    * `-j` or `--json` – Print a raw JSON from OpenWeatherMap
+    * `-j` or `--json` – Print a flat JSON (*to be improved*)
     * `-J` or `--JSON` – Print a beautified JSON (*to be improved*)
+    * `-l` or `--location` – Overwrite the city for the particular program execution (*to be implemented*)
     * `-n` or `--no-feed-line` – Don't feed the line after the execution of the program 
+    * `-o S` or `--output=S` – Adjust the output data you want to be displayed (*to be implemented*)
+    * `-rj` or `--raw-json` – Print a raw JSON from OpenWeatherMap (*to be implemented*, it returns the result in international SI units only)
+    * `-t T` or `--time=T` – Display weather data for the `N`th day after today (*to be implemented*, `N` may be negative, so then it would display weather from `N` days ago)
+    * `-T` or `--token` – Overwrite the OpenWeather API token for the particular program execution (*to be implemented*)
     * `-u N` or `--units=N` – Change output units (`N = [0, 1, 2, 3]`, see the list of values below)
 
-Available units (`N` values):
+Available locales (`N` values):
 - `N = 0` – :united_nations: International SI units (Kelvin, m/s and hPa)
 - `N = 1` – :eu: Metric units (Celsius, km/h and hPa; m/s on JSON output)
 - `N = 2` – :us: US Imperial units (Fahrenheit, mph and psi; hPa on JSON output) 
 - `N = 3` – :uk: UK Imperial units (Celsius, mph and mb) – so far it works like `N=1` on JSON output
+
+## Examples
+
+- `fpweather` shows weather of your city using your OpenWeatherMap API token in a compact readable format 
+- `fpweather -c` launches config
+- `fpweather -u 3` displays weather info of your city using the British locale (Celsius, mph, millibars) 
 
 ## More info
 
--- WeatherEngine.pas
+++ WeatherEngine.pas
@@ -18,6 +18,32 @@ implementation
 
 uses SysUtils;
 
+function isInInterval(x : Integer; a1, a2 : Extended) : Boolean;
+begin
+    Result := (x >= a1) and (x <= a2);
+end;
+
+function getWindDirection16(angle : Integer) : String;
+begin
+         if isInInterval(angle, -11.25,  11.25) then Result := 'N'
+    else if isInInterval(angle,  11.25,  33.75) then Result := 'NNE'
+    else if isInInterval(angle,  33.75,  56.25) then Result := 'NE'
+    else if isInInterval(angle,  56.25,  78.75) then Result := 'ENE'
+    else if isInInterval(angle,  78.75, 101.25) then Result := 'E'
+    else if isInInterval(angle, 101.25, 123.75) then Result := 'ESE'
+    else if isInInterval(angle, 123.75, 146.25) then Result := 'SE'
+    else if isInInterval(angle, 146.25, 168.75) then Result := 'SSE'
+    else if isInInterval(angle, 168.75, 191.25) then Result := 'S'
+    else if isInInterval(angle, 191.25, 213.75) then Result := 'SSW'
+    else if isInInterval(angle, 213.75, 236.25) then Result := 'SW'
+    else if isInInterval(angle, 236.25, 258.75) then Result := 'WSW'
+    else if isInInterval(angle, 258.75, 281.25) then Result := 'W'
+    else if isInInterval(angle, 281.25, 303.75) then Result := 'WNW'
+    else if isInInterval(angle, 303.75, 326.25) then Result := 'NW'
+    else if isInInterval(angle, 326.25, 348.75) then Result := 'NNW'
+    else Result := 'N';
+end;
+
 function getRequest(addr : String) : String;
 begin
     Result := '{}';
@@ -55,6 +81,7 @@ var
     pressure : Integer;
     humidity : Integer;
     windspeed : Extended;
+    windangle : Integer;
 begin
     //Result := 'A more beautified look of the weather info is coming soon! ^_^';
     jData := GetJSON(getRequest(str));
@@ -64,11 +91,12 @@ begin
     pressure := jData.GetPath('main').GetPath('pressure').AsInteger;
     humidity := jData.GetPath('main').GetPath('humidity').AsInteger;
     windspeed := jData.GetPath('wind').GetPath('speed').AsFloat;
+    windangle := jData.GetPath('wind').GetPath('deg').AsInteger;
     Result := 'Now in '+location+': '+desc+', '
         +FloatToStr(degrees)+loc.DegreesUnit+', '
         +Format('%.2f', [(pressure)/loc.PressureCoef])+loc.PressureUnit+', '
-        +'humidity '+IntToStr(humidity)+'%, '
-        +'wind speed '+Format('%.2f', [(windspeed)*loc.SpeedCoef])+loc.SpeedUnit+'.';
+        +IntToStr(humidity)+'% humid, '
+        +Format('%.2f', [(windspeed)*loc.SpeedCoef])+loc.SpeedUnit+' '+getWindDirection16(windangle)+' wind.';
 end;
 
 end.
